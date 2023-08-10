1f70cad99d678d2d368117c703b5976870be8a90
highjune <highjune37@gmail.com>
2023-08-11 00:20:54 +0900

    쓰레드 로컬의 동기화를 적용해서 로그 처리기의 동시성 문제 해결

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
 
     @Bean
-    public LogTrace logTrace() {
-        return new FieldLogTrace();
+    public LogTrace logTrace() { // 각 Controller, Service, Repository 에서 주입받음
+//        return new FieldLogTrace(); // 동시성 문제O
+        return new ThreadLocalLogTrace();  // 동시성 문제X. ThreadLocal 로 해결
     }
 }
